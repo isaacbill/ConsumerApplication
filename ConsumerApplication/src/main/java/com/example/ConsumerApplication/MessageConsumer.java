30405fa5117159dcb1ae5f6e5717945f4faf758b
Mogaka1994 <isaacbill247@gmail.com>
2023-02-08 08:54:34 +0300

    second application

---
+++ ConsumerApplication/src/main/java/com/example/ConsumerApplication/MessageConsumer.java
@@ -0,0 +1,17 @@
+package com.example.ConsumerApplication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class MessageConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageConsumer.class);
+
+    @JmsListener(destination = "${spring.artemis.embedded.queues}")
+    public void messageListener(String message){
+        LOGGER.info("Message received, {}",message);
+    }
+}
